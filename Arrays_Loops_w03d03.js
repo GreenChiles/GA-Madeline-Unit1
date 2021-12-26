1ac87f5798606789468106b0509daa7578bb85f7
GreenChiles <simslj98@gmail.com>
2021-12-26 12:07:24 -0700

    Fizz Buzz answered

--- Arrays_Loops_w03d03.js
+++ Arrays_Loops_w03d03.js
@@ -21,6 +21,17 @@ for (let i = 0; i<= 200; i++){
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
+for (let i = 1; i<= 100; i++){
+    if (i % 15 == 0){
+        console.log(i + " FizzBuzz")
+    } else if (i % 5 == 0){
+        console.log(i + " buzz")
+    } else if (i % 3 == 0){
+        console.log (i + " Fizz")
+    } else {
+        console.log(i)
+    }
+}
 
 
 ////////////////////////////////
