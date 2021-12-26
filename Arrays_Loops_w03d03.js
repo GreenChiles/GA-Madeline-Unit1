c76dea777ef25b3788b858a1ae3a5e83ab87ecf3
GreenChiles <simslj98@gmail.com>
2021-12-26 14:28:39 -0700

    Methods Revisited Answered

--- Arrays_Loops_w03d03.js
+++ Arrays_Loops_w03d03.js
@@ -69,11 +69,52 @@ ninjaTurtles = ["Dontalello", "Leonardo", "Raphael", "Michaelangelo"]
 for (const elem of ninjaTurtles){
     console.log(elem.toUpperCase() )
 }
+
 ////////////////////////////////
 // Methods, Revisited
 ////////////////////////////////
 
+const favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle',
+ 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story',
+  'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 
+  'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 
+  'Imitation of Life', 'Snatch', 'Fast and Furious'];
+  console.log(favMovies)
+  console.log(favMovies.indexOf("Titanic"))
+  favMovies.sort() // sorts alphabetically
+  console.log(favMovies)
+  favMovies.pop()
+  console.log(favMovies)
+  favMovies.push("Guardians of the Galaxy")
+  console.log(favMovies)
+  favMovies.reverse()
+  console.log(favMovies)
+  favMovies.shift()
+  console.log(favMovies)
+  favMovies.unshift() // doesnt add anything, with an element. Unshift adds stuff
+  //to the front of the array
+  console.log(favMovies)
+  console.log(favMovies.indexOf("Django Unchained")) //index:14
+  favMovies.splice(14,1, "Avatar")
+  console.log(favMovies)
+  let whatIsTheMiddleOfArray = Math.ceil(favMovies.length / 2)
+    console.log(whatIsTheMiddleOfArray) //index 9
 
+    const firstHalf = favMovies.slice(0, whatIsTheMiddleOfArray)
+    console.log(firstHalf)// did this one anyways
+    const secondHalf = favMovies.slice(-whatIsTheMiddleOfArray)
+    console.log(secondHalf) //This one is the last half
+    //So Math.ceil makes sure that the division would always round up, thus
+    //protecting me from errors, if it couldnt be split evenily
+    // the array.length / 2 is what cuts it in half. 
+// source: flaviocopes.com/how-to-cut-array-half-javascript/
+console.log(favMovies.indexOf("Fast and Furious")) //Index: 13
+//No? i get 13. I dont recall removing it.
+console.log(favMovies.indexOf("Django Unchained")) //index: -1
+// However if we do that on one that doesnt exist anymore, I
+//get a index of -1 (showing its nonexistent)
+//12. Maybe because its just the name it not any vaules that it is holding.
+// I think Let would just make the vaules changable, but im not sure now.
 ////////////////////////////////
 // Where is Waldo
 ////////////////////////////////
