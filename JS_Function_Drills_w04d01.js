80a80102ffb09757453b9cf1e8e9f533a556d8fc
GreenChiles <simslj98@gmail.com>
2021-12-27 12:36:25 -0700

    palindrome completed

--- JS_Function_Drills_w04d01.js
+++ JS_Function_Drills_w04d01.js
@@ -9,3 +9,20 @@
 // return is like console.log, but its on hold till its called. 
 // console.log runs immediattly when its written. Fun fact if you console.log
 // a console.log you will get an extra 'undefined' with your output.
+
+const palindrome = (str) => {
+var re = /[^A-Za-z0-9]/g
+str = str.toLowerCase().replace(re, '')
+var len = str.length
+for(var i = 0; i < len/2; i++){
+    if (str[i] !== str[len - 1 - i]){
+        return false
+    }
+}
+return true
+}
+
+console.log(palindrome("racecar"))
+console.log(palindrome("overflow"))
+
+//source: freecodecamp.org/news/two-ways-to-check-for-palindromes-in-javascript-64fea8191fd7/
