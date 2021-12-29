9e1ca09fccd89b791eb0aceeeaed521dbb347861
GreenChiles <simslj98@gmail.com>
2021-12-29 14:55:26 -0700

    Count Odd Numbers completed

--- JS_Problems_Pot_Mad_w04d02.js
+++ JS_Problems_Pot_Mad_w04d02.js
@@ -12,3 +12,41 @@ if(employed == true && vacationing == true){
 }
 }
 console.log(setAlarm(true, false)) //true
+
+
+// const countOddNumbers = (num) => {
+//     for (let i = 1; i <= num; i++){
+//         if (i % 2 !== 0){
+//             //return i
+//             console.log(i)
+//         } 
+//     }
+// }
+// //Console.log vs return....Question for office hours
+// wait, is it because return always stop for-loops?? 
+//stackoverflow.com/questions/11714503/does-return-stop-a-loop
+
+// countOddNumbers(20)
+//console.log(countOddNumbers(20))
+
+
+  const countOddNumbers = (num) => {
+    let count = 0
+    for (let i = 1; i <= num; i++){
+         if (i % 2 !== 0){
+             count++
+         } 
+     }
+     return count
+ }
+
+
+ console.log(countOddNumbers(20))
+// Shout out to my classmate Luke Fleming, He reallly helped me get this one
+// i was stuck on it for hours!
+
+
+
+
+
+
